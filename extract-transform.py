98afe6b9a11a12dcf23d2ffcc8fc1e98a482b629
Josh <joshsms56@gmail.com>
2025-10-06 21:54:39 +1100

    started transform code - WIP

---
+++ extract-transform.py
@@ -0,0 +1,184 @@
+import requests
+import os
+import pandas as pd
+from dotenv import load_dotenv
+
+load_dotenv()
+
+params = {
+    "language": "en-AU",
+    "region": "AU"
+}
+
+headers = { 
+    "accept": "application/json",
+    "Authorization": f"Bearer {os.environ.get('API_TOKEN')}"
+}
+
+now_playing_resp = requests.get(
+    url="https://api.themoviedb.org/3/movie/now_playing", 
+    headers=headers, 
+    params=params
+    )
+
+upcoming_resp = requests.get(
+    url="https://api.themoviedb.org/3/movie/upcoming", 
+    headers=headers, 
+    params=params
+    )
+
+if now_playing_resp.status_code == 200:
+    now_playing_json = now_playing_resp.json()
+else:
+    raise Exception(
+        f"""
+        Failed to extract now playing movie data from The Movies Database API. 
+        Status Code {now_playing_resp.status_code}. 
+        Response: {now_playing_resp.text}
+        """
+    )
+
+if upcoming_resp.status_code == 200:
+    upcoming_json = upcoming_resp.json()
+else:
+    raise Exception(
+        f"""
+        Failed to extract upcoming movie data from The Movies Database API. 
+        Status Code {upcoming_resp.status_code}. 
+        Response: {upcoming_resp.text}
+        """
+    )
+
+now_playing_total_pages = now_playing_json.get("total_pages")
+upcoming_total_pages = upcoming_json.get("total_pages")
+
+now_playing_data = now_playing_json.get("results")
+upcoming_data = upcoming_json.get("results")
+
+if now_playing_total_pages > 1:
+    for page_num in range(2, now_playing_total_pages + 1):
+        params.update({"page":page_num})
+        response = requests.get(url="https://api.themoviedb.org/3/movie/now_playing", headers=headers, params=params)
+        if response.status_code == 200:
+            response_data = response.json()
+            now_playing_data.extend(response_data.get("results"))
+        else:
+            raise Exception(
+                f"""
+                Failed to extract now playing movie data from The Movies Database API. 
+                Status Code {now_playing_resp.status_code}. 
+                Response: {now_playing_resp.text}
+                """
+            )
+else:
+    pass
+
+if upcoming_total_pages > 1:
+    for page_num in range(2, upcoming_total_pages + 1):
+        params.update({"page":page_num})
+        response = requests.get(url="https://api.themoviedb.org/3/movie/upcoming", headers=headers, params=params)
+        if response.status_code == 200:
+            response_data = response.json()
+            upcoming_data.extend(response_data.get("results"))
+        else:
+            raise Exception(
+                f"""
+                Failed to extract upcoming movie data from The Movies Database API. 
+                Status Code {upcoming_resp.status_code}. 
+                Response: {upcoming_resp.text}
+                """
+            )
+else:
+    pass
+
+# convert to df
+genres_df = pd.read_csv("data/genres.csv")
+now_playing_df = pd.DataFrame(data=pd.json_normalize(now_playing_data))
+upcoming_df = pd.DataFrame(data=pd.json_normalize(upcoming_data))
+
+# drop columns
+now_playing_df = now_playing_df.drop(
+    columns=[
+        "backdrop_path",
+        "original_title",
+        "overview",
+        "poster_path",
+        "video"
+    ]
+)
+
+upcoming_df = upcoming_df.drop(
+    columns=[
+        "backdrop_path",
+        "original_title",
+        "overview",
+        "poster_path",
+        "video",
+        "vote_average",
+        "vote_count"
+    ]
+)
+
+# rename columns
+genres_df = genres_df.rename(
+    columns={
+        "id":"genre_id",
+        "name":"genre"
+        }
+)
+
+now_playing_df = now_playing_df.rename(
+    columns={
+        "adult":"is_adult",
+        "genre_ids":"genre",
+        "id": "movie_id",
+        "original_language": "language",
+        "popularity":"popularity_score",
+        "vote_average": "rating_average",
+        "vote_count":"rating_count"
+    }
+)
+
+upcoming_df = upcoming_df.rename(
+    columns={
+        "adult":"is_adult",
+        "genre_ids":"genre",
+        "id": "movie_id",
+        "original_language": "language",
+        "popularity":"popularity_score",
+        "vote_average": "rating_average",
+        "vote_count":"rating_count"
+    }
+)
+
+# reorder columns
+now_playing_column_order = ['movie_id', 'title', 'release_date', 'genre', 'language', 'is_adult', 'popularity_score', 'rating_count', 'rating_average']
+now_playing_df = now_playing_df[now_playing_column_order]
+
+upcoming_column_order = ['movie_id', 'title', 'release_date', 'genre', 'language', 'is_adult', 'popularity_score']
+upcoming_df = upcoming_df[upcoming_column_order]
+
+# rearrange date
+now_playing_df['release_date'] = pd.to_datetime(now_playing_df['release_date']).dt.strftime("%d-%m-%Y")
+upcoming_df['release_date'] = pd.to_datetime(upcoming_df['release_date']).dt.strftime("%d-%m-%Y")
+
+# data types
+
+# join with genre
+now_playing_df_exploded = now_playing_df.explode("genre")
+now_playing_df_exploded["genre"] = pd.to_numeric(now_playing_df_exploded["genre"]).astype(dtype="Int64")
+
+genres_df = genres_df["genre_id"].astype(dtype="Int64")
+
+#print(now_playing_df_exploded.head())
+#print(genres_df.dtypes)
+
+now_playing_df_merged = pd.merge(left=now_playing_df_exploded, right=genres_df, left_on="genre", right_on="genre_id")
+print(now_playing_df_merged.head())
+
+pd.set_option('display.max_columns', None)  # Show all columns
+#pd.set_option('display.width', None)        # Don't wrap to fit terminal
+
+#print(genres_df.head())
+#print(now_playing_df.head())
+#print(upcoming_df.head())
